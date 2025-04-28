4e8b7a1bcf31d9fb749eaeadad571ba49c53f10a
ZQ <zqzqsb@gmail.com>
2025-04-29 00:06:29 +0800

    feat(app): modify session to adapt redis cluster

--- app/user/biz/router/middleware/session.go
+++ app/user/biz/router/middleware/session.go
@@ -11,13 +11,24 @@ import (
 
 func InitSession(h *server.Hertz) {
 	config := conf.GetConf()
+	
+	// 使用单机 Redis
 	store, err := redis.NewStore(10, "tcp",
 		config.Redis.Address,
 		config.Redis.Password,
 		[]byte("your-session-secret-key"))
+	
 	if err != nil {
 		panic(err)
 	}
+	
+	// 设置 cookie 选项
+	store.Options(sessions.Options{
+		Path:     "/",
+		MaxAge:   86400, // 1 天
+		HttpOnly: true,
+	})
+	
 	h.Use(sessions.New("hertz-session", store))
 	log.Println("init session success")
 }
